72c56f86d60290c04e72da773f795308d36a2d46
Aman priyansh <amanpriyansh73@gmail.com>
2026-04-21 11:39:22 +0530

    added tree generation for commit

--- commit.c
+++ commit.c
@@ -196,5 +196,10 @@ int head_update(const ObjectID *new_commit) {
 
 
 int commit_create(const char *message, ObjectID *id_out) {
+    ObjectID tree_id;
+
+    if (tree_from_index(&tree_id) != 0)
+        return -1;
+
     return -1;
 }
